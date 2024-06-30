838bb9ebf974880e8bd3d70aecf9077fdc3a455c
MKA1500 <maciejkabsch@gmail.com>
2024-06-30 15:02:29 +0200

    init

---
+++ src/test/java/com/demo/juneTwentyFour/JuneTwentyFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.juneTwentyFour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuneTwentyFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
